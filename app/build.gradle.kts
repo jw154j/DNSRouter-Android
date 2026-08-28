8329126a446ef08541ba2416a7cd32a2ff79e198
jw154j <jw154j@me.com>
2026-08-28 05:55:01 -0500

    v1.98 cont.
    expanding transport protocols. correcting tun

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android {
     namespace = "com.jason.dnsrouter"
-    compileSdk = 35
+    compileSdk = 37
     defaultConfig {
         applicationId = "com.jason.dnsrouter"
         minSdk = 29
@@ -15,10 +15,10 @@ android {
 kotlin { jvmToolchain(17) }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.activity:activity-ktx:1.13.0")
+    implementation("androidx.appcompat:appcompat:1.8.0")
+    implementation("com.google.android.material:material:1.14.0")
     implementation("androidx.security:security-crypto:1.1.0")
-    implementation("com.google.android.gms:play-services-cronet:18.1.0")
+    implementation("com.google.android.gms:play-services-cronet:18.1.1")
 }
